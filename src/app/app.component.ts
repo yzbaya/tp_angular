f0035fe8511324442f7a66de45936750ca419077
yzbaya <101043195+yzbaya@users.noreply.github.com>
2022-10-05 21:10:08 +0200

    first one

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, ResolvedReflectiveFactory } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>
+  {{title}}
+  </h1>
+  <p>  
+  lenght: {{title.length}}
+  </p>
+  <p>
+  Reserve: {{getReversed(title)}}
+  </p>
+
+  `,
+
+styles:[]
+})
+export class AppComponent {
+  title = 'APP Works!';
+  getReversed(str:string){
+    let reversed ='';
+    for(let i = str.length - 1; i >= 0; i--){
+
+      reversed+=str.substring(i,i+1);
+    }
+ return reversed;
+  }
+}
